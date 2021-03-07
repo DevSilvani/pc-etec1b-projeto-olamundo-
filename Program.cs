ab996926b11d373fe91edee6c30b3c61b431d5e6
SILVANI CUSTODIO DE OLIVEIRA <silvani.oliveira@etec.sp.gov.br>
2021-03-07 12:00:31 -0300

    C# pronto

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace pc_etec1b_projeto_olamundo_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá usuario, eu me chamo Silvani Oliveira!");
+            Console.ReadKey();
+        }
+    }
+}
